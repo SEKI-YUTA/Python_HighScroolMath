ee8f5342993533f8430fc5af8e4e410f2e896d19
SEKI-YUA <zard.since1991@gmail.com>
2023-06-08 10:23:19 +0900

    0608 morning

--- graph.py
+++ graph.py
@@ -72,13 +72,33 @@ import math
 # plt.show()
 
 #
-# 図形の平行移動(同次座標を使う)
+# # 図形の平行移動(同次座標を使う)
+# p = np.matrix([[1, 3, 3, 1], [1, 1, 2, 1], [1, 1, 1, 1]])
+# # 変換座標
+# A = np.matrix([[1,0,2], [0, 1, 3], [0, 0, 1]])
+# p2 = A * p
+# p = np.array(p)
+# p2 = np.array(p2)
+# plt.plot(p[0], p[1])
+# plt.plot(p2[0], p2[1])
+# plt.axis("equal")
+# plt.grid(color="0.8")
+# plt.show()
+
+#
+# 図形を平行移動してから回転させる
 p = np.matrix([[1, 3, 3, 1], [1, 1, 2, 1], [1, 1, 1, 1]])
-# 変換座標
-A = np.matrix([[1,0,2], [0, 1, 3], [0, 0, 1]])
-p2 = A * p
+
+A = np.matrix([[1, 0, 2], [0, 1, 3], [0, 0, 1]])
+th = np.radians(90)
+B = np.matrix([[np.cos(th), np.sin(-th), 0], [np.sin(th), np.cos(th), 0], [0, 0, 1]])
+
+# p2 = A * B * p
+X = B * A
+p2 = X * p
 p = np.array(p)
 p2 = np.array(p2)
+
 plt.plot(p[0], p[1])
 plt.plot(p2[0], p2[1])
 plt.axis("equal")
